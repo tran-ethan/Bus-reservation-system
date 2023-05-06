d70194ec63f97839abf322cbaa3fc3c341ba04a1
exisodd <ethantran004@yahoo.ca>
2023-05-05 22:29:57 -0400

    Client editing profile logic

--- src/main/java/com/busreservationsystem/controllers/ClientEditProfileControllerClient.java
+++ src/main/java/com/busreservationsystem/controllers/ClientEditProfileControllerClient.java
@@ -6,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,17 +31,60 @@ public class ClientEditProfileControllerClient extends ClientController implemen
 
     @FXML
     private Label newEmail;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField passwordField;
+
+    private Client client;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Client client = Database.getCurrentClient();
+        client = Database.getCurrentClient();
         setCredentials();
         oldUsername.setText(client.getUsername());
         oldName.setText(client.getFullName());
         oldEmail.setText(client.getEmail());
     }
 
+    /**
+     * Handles changing user credentials, handles appropriate error messages for user interaction.
+     * Is called when "Save" button is clicked
+     * @param event The source of the event.
+     */
     @FXML
     void save(ActionEvent event) {
+        // Update user credentials
+        client.setUsername(usernameField.getText());
+        client.setFullName(nameField.getText());
+        client.setEmail(emailField.getText());
+        client.setPassword(passwordField.getText());
+        // Reloads page to update left panel
+        loadFXML("ClientEditProfile");
+    }
 
+    /**
+     * Handles the text change event for the text fields - is called everytime a character is inputted into a field.
+     * Updates the corresponding labels based on the source of the field that has been changed.
+     * @param event The source of the event.
+     */
+    @FXML
+    void newCredentialsChange(KeyEvent event) {
+        TextField field = (TextField) event.getSource();
+        String fieldId = field.getId();
+        String newValue = field.getText();
+        switch (fieldId) {
+            case "usernameField" -> newUsername.setText(newValue);
+            case "nameField" -> newName.setText(newValue);
+            case "emailField" -> newEmail.setText(newValue);
+        }
     }
 }
